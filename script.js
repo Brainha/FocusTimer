79e547d721ec7b30139683fa7c0e600fd529154d
Braian <braiannickolas12@gmail.com>
2024-02-28 05:31:48 -0300

    Aula213
    Iniciando a logica para o contador

--- script.js
+++ script.js
@@ -4,12 +4,33 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundON = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+let minutes
+const minutesDisplay = document.querySelector('.minutes')
+const secondDisplay = document.querySelector('.seconds')
+
+function countdown() {
+  setTimeout(function () {
+    let seconds = Number(secondDisplay.textContent)
+
+    if (seconds <= 0) {
+      seconds = 60
+    }
+
+
+    secondDisplay.textContent = seconds - 1
+
+    countdown()
+  }, 1000)
+}
 
 buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+
+  countdown()
+
 })
 
 buttonPause.addEventListener('click', function () {
@@ -34,4 +55,9 @@ buttonSoundOff.addEventListener('click', function () {
 buttonSoundON.addEventListener('click', function () {
   buttonSoundON.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function () {
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = minutes
 })
