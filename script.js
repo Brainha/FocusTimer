d0701b41706f35d82ad5fda9f954a349de0ab342
Braian <braiannickolas12@gmail.com>
2024-02-29 10:40:15 -0300

    Aula216
    Resetando o cronometro e entendendo escopos

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundON = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
-let minutes
 const minutesDisplay = document.querySelector('.minutes')
 const secondDisplay = document.querySelector('.seconds')
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut
 
 function resetControls() {
   buttonPlay.classList.remove('hide')
@@ -15,13 +16,18 @@ function resetControls() {
   buttonStop.classList.add('hide')
 }
 
+function resetTimer() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
+
 function updateTimerDisplay(minutes, seconds) {
   minutesDisplay.textContent = String(minutes).padStart(2, "0")
   secondDisplay.textContent = String(seconds).padStart(2, "0")
 }
 
 function countdown() {
-  setTimeout(function () {
+  TimeOut = setTimeout(function () {
     let seconds = Number(secondDisplay.textContent)
     let minutes = Number(minutesDisplay.textContent)
 
@@ -58,11 +64,12 @@ buttonPlay.addEventListener('click', function () {
 buttonPause.addEventListener('click', function () {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
-
+  clearTimeout(TimeOut)
 })
 
 buttonStop.addEventListener('click', function () {
   resetControls()
+  resetTimer()
 })
 
 buttonSoundOff.addEventListener('click', function () {
@@ -77,6 +84,12 @@ buttonSoundON.addEventListener('click', function () {
 })
 
 buttonSet.addEventListener('click', function () {
-  minutes = prompt('Quantos minutos?')
+  let newMinutes = prompt('Quantos minutos?')
+  if (!newMinutes) {
+    resetTimer()
+    return
+  }
+
+  minutes = newMinutes
   updateTimerDisplay(minutes, 0)
-})
+}) 
